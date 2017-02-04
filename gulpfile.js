20a872294f67260afd11d0224ed5ea25eacb6e13
Jose Antonio Palacios Ramírez <josepio11.uma@gmail.com>
2017-02-04 18:10:39 +0100

    añadimo browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync').create();
-var concat = require('gulp-concat');
+//var concat = require('gulp-concat');
+var browserify = require('browserify');
+var tap = require('gulp-tap');
+var buffer = require('gulp-buffer');
 
 var sassConfig = {
   compileSassTaskName: 'compile-sass',
@@ -15,7 +18,7 @@ var sassConfig = {
 var jsConfig = {
   concatJsTaskName: 'concat-js',
   watchFiles: './src/js/*.js',
-  src: './src/js/*.js',
+  src: './src/js/main.js',
   concatFile: 'main.js',
   dest: './dist'
 }
@@ -30,10 +33,10 @@ gulp.task("default",[sassConfig.compileSassTaskName,jsConfig.concatJsTaskName],f
   });
 
   //Cuando haya cambios en archivos js, los Concatena
-  gulp.watch(sassConfig.watchFiles,["sassConfig.compileSassTaskName"]);
+  gulp.watch(sassConfig.watchFiles,[sassConfig.compileSassTaskName]);
 
   //Cuando haya cambio compila sass
-  gulp.watch(jsConfig.watchFiles,jsConfig.concatJsTaskName);
+  gulp.watch(jsConfig.watchFiles,[jsConfig.concatJsTaskName]);
   gulp.watch("./*.html",function(){
     browserSync.reload();
     notify().write("Navegador recargado");
@@ -58,7 +61,12 @@ gulp.task("compile-sass", function(){
 //Concatena JS
 gulp.task("concat-js",function(){
   gulp.src(jsConfig.src)
-  .pipe(concat(jsConfig.concatFile))
+  .pipe(tap(function(file){ //Para cada archivo seleccionado
+    //lo pasamos por browserify
+    file.contents = browserify(file.path).bundle();
+  }))
+  .pipe(buffer()) //Convertimos a buffer para que funcione el siguiente pipe
+  //.pipe(concat(jsConfig.concatFile))
   .pipe(gulp.dest(jsConfig.dest))
   .pipe(notify("JS Concatenado"))
   .pipe(browserSync.stream());
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+var $ = require('jquery');
+
+$('publication-list').fadeOut(8000);
+
+console.log("Hello form app.js");
